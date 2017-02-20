e61fdf5359a8aa399dc87c388fbdb6ab8e9333d8
Cheah Hong Loo <Cheah Hong Loo>
2017-02-20 08:34:20 +0800

    'download-file'

--- app/src/main/java/com/example/user/preconsumerapp/MainActivity.java
+++ app/src/main/java/com/example/user/preconsumerapp/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.user.preconsumerapp;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -19,20 +22,27 @@ import com.android.volley.toolbox.Volley;
 import com.google.zxing.integration.android.IntentIntegrator;
 import com.google.zxing.integration.android.IntentResult;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.net.URLConnection;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView verResult,publicKeyString,signatureString,batchID;
+    TextView verResult,encryptedEncodedJson,encryptedEncodedKey,batchID;
     Button btn,btnScan,btnPost;
     RequestQueue queue;
     JsonObjectRequest postRequest;
     String message;
-
+    String encryptedData,encryptedKey,filePath;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,8 +50,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         verResult = (TextView)findViewById(R.id.verifyResult);
-        publicKeyString = (TextView)findViewById(R.id.pubKey);
-        signatureString = (TextView)findViewById(R.id.signature);
+        encryptedEncodedJson = (TextView)findViewById(R.id.encryptedEncodedJson);
+        encryptedEncodedKey = (TextView)findViewById(R.id.encryptedEncodedKey);
         btn = (Button)findViewById(R.id.btn);
 
 
@@ -51,34 +61,28 @@ public class MainActivity extends AppCompatActivity {
 
         queue = Volley.newRequestQueue(getApplicationContext());
 
-
+        DownloadFile dl = new DownloadFile();
+        dl.execute();
+        Log.d("filepath?", filePath);
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 queue = Volley.newRequestQueue(getApplicationContext());
 
-                final String url  = "http://192.168.56.1:8080/";
+                final String url  = "http://192.168.212.90:7080/";
 
                 JsonObjectRequest getRequest = new JsonObjectRequest(Request.Method.GET, url, (String)null, new Response.Listener<JSONObject>() {
                     @Override
                     public void onResponse(JSONObject response) {
                        // Log.d("response", response.toString());
                         try{
-                           Log.d("response1",response.getString("publicKey"));
-                            Log.d("response2",response.getString("encryptedLocation"));
-                            String pubKey = response.getString("publicKey");
-                            String signature = response.getString("encryptedLocation");
-                            VerSig verify = new VerSig();
-                            Boolean verified = verify.verifySignature(pubKey,signature);
-                            if(verified){
-                                publicKeyString.setText("PublicKey: "+pubKey);
-                                signatureString.setText("Signature: "+signature);
-                                verResult.setText("Verified: "+verified);
-                            }else {
-                                publicKeyString.setText("PublicKey: "+pubKey);
-                                signatureString.setText("Signature: "+signature);
-                                verResult.setText("Verified: "+verified);
-                            }
+                            Log.d("Obj1",response.getString("encodedEncryptedJson"));
+                            Log.d("Obj2",response.getString("encodedEncryptedKey"));
+                            encryptedData = response.getString("encodedEncryptedJson");
+                            encryptedKey = response.getString("encodedEncryptedKey");
+                            encryptedEncodedJson.setText("Encrypted Json: " + response.getString("encodedEncryptedJson"));
+                            encryptedEncodedKey.setText("Encrypted Key: " +response.getString("encodedEncryptedKey"));
+                            verResult.setText("Verified: ");
                         }
                         catch (Exception e){
                             e.printStackTrace();
@@ -175,6 +179,76 @@ public class MainActivity extends AppCompatActivity {
         if (scanResult != null) {
             Log.d("result",scanResult.toString());
             batchID.setText(scanResult.toString());
+        }
+    }
+
+    class DownloadFile extends AsyncTask<String,String,String>
+    {
+        ProgressDialog loading;
+        String FILE_URL="https://atos-x509certs-jiahong96.c9users.io/cacert.pem";
+        String FILE_Name="cacert";
+        String temp;
+
+        @Override
+        protected void onPreExecute() {
+
+            super.onPreExecute();
+            loading = ProgressDialog.show(getApplicationContext(),"Loading...","Wait...", true, true);
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            filePath=result;
+            loading.dismiss();
+        }
+
+        @Override
+        protected String doInBackground(String... file_path) {
+            int readBytes;
+
+            String path;
+
+//                temp= Environment.getExternalStorageDirectory().getPath()+"/"+FILE_Name+".pem";
+//                temp.replaceAll("\\s"," ");
+//                File f = new File(temp);
+//                Log.d("bba",f.toString());
+//                if(f.exists())
+//                {
+//                    Log.d("bb","bb");
+//                    path=f.toString();
+//                    return path;
+//                }
+//
+//                else{
+                    try{
+                        URL url = new URL(FILE_URL);
+                        URLConnection connection=url.openConnection();
+                        long fileLength=connection.getContentLength();
+
+                        InputStream input = new BufferedInputStream(url.openStream(),10*1024);
+                        OutputStream output = new FileOutputStream(Environment.getExternalStorageDirectory().getPath()+"/"+FILE_Name+".pem");
+                        byte data[]=new byte[1024];
+                        long totalBytes = 0;
+
+                        while((readBytes=input.read(data))!=-1)
+                        {
+                            totalBytes=totalBytes+readBytes;
+                            Long percentage = (totalBytes*100)/fileLength;
+                            publishProgress(String.valueOf(percentage));
+                            output.write(data,0,readBytes);
+                        }
+
+                        output.flush();
+                        output.close();
+                        input.close();
+                        path = Environment.getExternalStorageDirectory().toString()+"/"+FILE_Name+".pem";
+                        Log.d("got path?",path);
+                        return path;
+                    }catch(Exception e){
+                        Log.d("Error",e.getMessage());
+                    }
+//                }
+            return null;
         }
     }
 
